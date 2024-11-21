62b306e822b98594617f2315dbc71474590866ca
Jukka Siukola <jukka.siukola@sofigate.com>
2024-11-21 10:58:25 +0000

    Detail view added

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,7 +1,9 @@
 sap.ui.define([
     "sap/ui/core/UIComponent",
-    "student402/com/sap/training/ux402/listdetail/ux402listdetail/model/models"
-], (UIComponent, models) => {
+    "sap/ui/Device",
+    "student402/com/sap/training/ux402/listdetail/ux402listdetail/model/models",
+    "student402/com/sap/training/ux402/listdetail/ux402listdetail/controller/ListSelector"
+], (UIComponent, Device, models, ListSelector) => {
     "use strict";
 
     return UIComponent.extend("student402.com.sap.training.ux402.listdetail.ux402listdetail.Component", {
@@ -16,11 +18,14 @@ sap.ui.define([
             // call the base component's init function
             UIComponent.prototype.init.apply(this, arguments);
 
-            // set the device model
-            this.setModel(models.createDeviceModel(), "device");
+            // instantiation of the listselector
+            this.oListSelector = new ListSelector();
 
             // enable routing
             this.getRouter().initialize();
+
+            // set the device model
+            this.setModel(models.createDeviceModel(), "device");
         }
     });
 });
--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,10 +1,17 @@
 sap.ui.define([
-  "sap/ui/core/mvc/Controller"
-], (Controller) => {
+  "./BaseController",
+  "sap/ui/model/json/JSONModel"
+], (BaseController, JSONModel) => {
   "use strict";
 
-  return Controller.extend("student402.com.sap.training.ux402.listdetail.ux402listdetail.controller.List", {
-      onInit() {
-      }
+  return BaseController.extend("student402.com.sap.training.ux402.listdetail.ux402listdetail.controller.App", {
+    onInit() {
+
+      var oViewModel = new JSONModel({
+        layout: "OneColumn"
+      });
+
+      this.getView().setModel(oViewModel, "mainView");
+    }
   });
 });
